77a3ad0b6ff404568bc149c3cf7606165752cd9b
dvelazquez <dvelazquezb@gmail.com>
2016-01-24 12:00:43 -0600

    Added Mesh

--- main.c
+++ main.c
@@ -75,30 +75,32 @@ int main( int argc, char *argv[ ] )
     cvInitFont(&font,CV_FONT_HERSHEY_SIMPLEX|CV_FONT_ITALIC, hScale,vScale,0,lineWidth,8);   //added 8 (line type)
 
     Picture = cvQueryFrame( capture );
-    Gray= cvCreateImage( cvGetSize(Picture), IPL_DEPTH_8U, 1 );  // Do not create the image everytime inside the main loop
-    HSV= cvCreateImage( cvGetSize(Picture), IPL_DEPTH_8U, 3 );  // Do not create the image everytime inside
+
+    Gray= cvCreateImage( cvGetSize(Picture), IPL_DEPTH_8U, 1 );  // Do not create the image everytime 
+    HSV= cvCreateImage( cvGetSize(Picture), IPL_DEPTH_8U, 3 );  // Do not create the image everytime 
     HSV_Result= cvCreateImage( cvGetSize(Picture), IPL_DEPTH_8U, 1);  // Results is mono image
 
     cvNamedWindow ("Camera",1); // added 1 WINDOW_AUTOSIZE = camera capture size (640x480)
     while (key != 'q'){
                 Picture = cvQueryFrame( capture );
 
-//                cvPutText (Picture, argv[1],cvPoint(180,470), &font, cvScalar(255,0,0,0));
+		double w;
+		double h;
+		cvGetSize(Picture);
+		cvSize(w,h);
+		printf("W= %d  H=%d\n",w,h);
+
                 cvPutText (Picture,"AFCS",cvPoint(180,470), &font, cvScalar(255,0,0,0));   //added last 0
-                // Speedo cross
-                cvLine(Picture,cvPoint(315, 130),cvPoint(315,150),RED,1,8,1); //added shift=1 ?
-                cvLine(Picture,cvPoint(305, 140),cvPoint(325,140),RED,1,8,1);//added shift=1 ?
-                //  Temp cross
-                cvLine(Picture,cvPoint(95, 150),cvPoint(95,170),GREEN,1,8,1);//added shift=1 ?
-                cvLine(Picture,cvPoint(85, 160),cvPoint(105,160),GREEN,1,8,1);//added shift=1 ?
-                // Fuel cross
-                cvLine(Picture,cvPoint(525, 165),cvPoint(525,185),BLUE,1,8,1);//added shift=1 ?
-                cvLine(Picture,cvPoint(515, 175),cvPoint(535,175),BLUE,1,8,1);//added shift=1 ?
-                // Pick two opposites points to center the cluster
-                // Fuel Tank
-                cvRectangle(Picture, cvPoint(490,60), cvPoint(520,90), RED, 2,1,1);//added shift=1 ?
-                // Door
-                cvRectangle(Picture, cvPoint(230,265), cvPoint(265,300), BLUE, 2,1,1);//added shift=1 ?
+
+                // Let's draw a mesh to show coordinates for development
+		for (x=1;x<=6200; x=x+10){
+			cvLine(Picture,cvPoint(x, 1),cvPoint(x,680),GREEN,1,8,1); //added shift=1 ?			
+			cvPutText (Picture,"*",cvPoint(620,480), &font, cvScalar(255,0,0,0));   //added last 0
+//			printf("int %i",x);
+
+		}
+                cvLine(Picture,cvPoint(610, 470),cvPoint(615,475),RED,1,8,1);//added shift=1 ?
+                cvLine(Picture,cvPoint(1270, 1),cvPoint(1270,1200),RED,1,8,1);//added shift=1 ?
 
 		/* Now some processing to the image */
 //		cvCvtColor(Picture,HSV,CV_BGR2GRAY); // Convert color image to gray		
@@ -114,7 +116,7 @@ int main( int argc, char *argv[ ] )
 		point = (CvPoint *)CV_GET_SEQ_ELEM(CvPoint,contours,1);  // 1 is i
 		x=point->x;
 		y=point->y;
-		printf("Point X %i, Point Y %i\n", x, y);
+//		printf("Point X %i, Point Y %i\n", x, y);
                 cvPutText (Picture,"Here",cvPoint(x,y), &font, cvScalar(255,0,0,0));   //added last 0
 
 
@@ -123,8 +125,8 @@ int main( int argc, char *argv[ ] )
 
 
                 cvShowImage ("Camera", Picture);
-//                cvMoveWindow("Camera", 100, 50);
-                cvShowImage ("HSV", HSV_Result);
+                cvMoveWindow("Camera", 100, 50);
+//                cvShowImage ("HSV", HSV_Result);
 //                cvMoveWindow("HSV", 100, 50);
 
        key = cvWaitKey( 20 );    // Press Q to Quit
@@ -135,3 +137,22 @@ int main( int argc, char *argv[ ] )
     cvDestroyWindow( "Camera" );    // Destroy the window
     cvReleaseCapture( &capture );   // Release it or never close
     }
+
+
+
+/*
+Initial Squares, texts, etc...
+                //  Temp cross
+                cvLine(Picture,cvPoint(95, 150),cvPoint(95,170),GREEN,1,8,1);//added shift=1 ?
+
+                cvLine(Picture,cvPoint(85, 160),cvPoint(105,160),GREEN,1,8,1);//added shift=1 ?
+                // Fuel cross
+                cvLine(Picture,cvPoint(525, 165),cvPoint(525,185),BLUE,1,8,1);//added shift=1 ?
+                cvLine(Picture,cvPoint(515, 175),cvPoint(535,175),BLUE,1,8,1);//added shift=1 ?
+                // Pick two opposites points to center the cluster
+                // Fuel Tank
+                cvRectangle(Picture, cvPoint(490,60), cvPoint(520,90), RED, 2,1,1);//added shift=1 ?
+
+                // Door
+                cvRectangle(Picture, cvPoint(230,265), cvPoint(265,300), BLUE, 2,1,1);//added shift=1 ?
+*/
