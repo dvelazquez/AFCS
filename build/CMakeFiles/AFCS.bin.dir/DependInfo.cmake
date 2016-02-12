4a016384566f013ed134a11a2fb5405f72a75023
dvelazquez <dvelazquezb@gmail.com>
2016-02-11 23:19:19 -0600

    Changed Makefile to CMakeLists.txt

---
+++ build/CMakeFiles/AFCS.bin.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pi/OpenCV/AFCS/src/ImageProcessing.c" "/home/pi/OpenCV/AFCS/build/CMakeFiles/AFCS.bin.dir/src/ImageProcessing.o"
+  "/home/pi/OpenCV/AFCS/src/Initialization.c" "/home/pi/OpenCV/AFCS/build/CMakeFiles/AFCS.bin.dir/src/Initialization.o"
+  "/home/pi/OpenCV/AFCS/src/Mesh.c" "/home/pi/OpenCV/AFCS/build/CMakeFiles/AFCS.bin.dir/src/Mesh.o"
+  "/home/pi/OpenCV/AFCS/src/main.c" "/home/pi/OpenCV/AFCS/build/CMakeFiles/AFCS.bin.dir/src/main.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/AFCS.bin.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AFCS.bin.dir/src/main.o"
+  "CMakeFiles/AFCS.bin.dir/src/Mesh.o"
+  "CMakeFiles/AFCS.bin.dir/src/ImageProcessing.o"
+  "CMakeFiles/AFCS.bin.dir/src/Initialization.o"
+  "AFCS.bin.pdb"
+  "AFCS.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/AFCS.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
